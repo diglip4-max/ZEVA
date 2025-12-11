88cb1b6b0569b1fb1603205aa34ec7b72f9f9b7f
Muskan913 <aroigupta367@gmail.com@gmail.com>
2025-12-11 16:46:20 +0530

    ok

--- pages/api/marketing/sms-send.js
+++ pages/api/marketing/sms-send.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
 
         const { hasPermission, error: permError } = await checkClinicPermission(
           clinicId,
-          "clinic_staff_management",
+          "clinic_marketing",
           "create",
           "SMS Marketing"
         );
