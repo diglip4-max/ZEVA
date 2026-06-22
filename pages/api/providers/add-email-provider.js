7fced37d42e9ab94e4fd376282ece63c7578bbea
Bajuddin Khan <bajuddinkhan786@gmail.com>
2026-06-22 13:07:10 +0530

    incoming smtp emails fetched

--- pages/api/providers/add-email-provider.js
+++ pages/api/providers/add-email-provider.js
@@ -111,6 +111,8 @@ export default async function handler(req, res) {
       smtpPassword,
     });
 
+    console.log("isValidSmtp:", isValidSmtp);
+
     if (!isValidSmtp) {
       return res.status(400).json({
         success: false,
