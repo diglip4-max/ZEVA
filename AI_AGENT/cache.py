a1b4736d9ff14a9cbc996639eb3116ba13c03d1f
diglip4-max <diglip4@gmail.com>
2026-07-04 13:56:43 +0530

    ok

--- AI_AGENT/cache.py
+++ AI_AGENT/cache.py
@@ -1,4 +1,7 @@
 # cache.py
+from dotenv import load_dotenv
+load_dotenv()
+
 import redis.asyncio as redis
 import os
 import json
@@ -10,7 +13,6 @@ redis_client = redis.from_url(
     protocol=2,
 )
 
-
 async def get_cache(key: str) -> dict | None:
     try:
         data = await redis_client.get(key)
@@ -19,7 +21,6 @@ async def get_cache(key: str) -> dict | None:
         return None
     except Exception:
         return None
-
 
 async def set_cache(key: str, value: dict, ttl_seconds: int) -> None:
     try:
